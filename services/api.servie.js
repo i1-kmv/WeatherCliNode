72fb97d1e168c9f1a0ca711e31ddb940ec328bc8
IYarovoy <IYarovoy@korusconsulting.ru>
2024-02-22 21:39:01 +0300

    finish

--- services/api.servie.js
+++ services/api.servie.js
@@ -1,6 +1,28 @@
-import https from "https";
 import {getKeyValue, TOKEN_DICTIONARY} from "./storage.service.js";
 import axios from "axios";
+
+const getIcon = (icon) => {
+    switch (icon.slice(0, -1)) {
+        case "01":
+            return "☀️";
+        case "02":
+            return "🌤️";
+        case "03":
+            return "☁️";
+        case "04":
+            return "☁️";
+        case "09":
+            return "🌧️";
+        case "10":
+            return "🌦️";
+        case "11":
+            return "🌩️";
+        case "13":
+            return "❄️";
+        case "50":
+            return "🌫️";
+    }
+}
 
 const getWeather = async (city) => {
     const token = await getKeyValue(TOKEN_DICTIONARY.token)
@@ -16,20 +38,6 @@ const getWeather = async (city) => {
         }
     });
     return data;
-    // const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-    // url.searchParams.append("q", city);
-    // url.searchParams.append("appId", token);
-    // url.searchParams.append("lang", "ru");
-    // url.searchParams.append("units", "metric");
-    // https.get(url, (res) => {
-    //     let data = "";
-    //     res.on("data", (chunk) => {
-    //         data += chunk;
-    //     })
-    //     res.on("end", () => {
-    //         console.log(JSON.parse(data));
-    //     })
-    // })
 }
 
-export { getWeather };
+export { getWeather, getIcon };
